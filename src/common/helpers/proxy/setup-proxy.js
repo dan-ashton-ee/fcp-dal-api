591fbc342db89aca88fdac901b67f35e97686f27
Robin Knipe <robin.knipe@defra.gov.uk>
2025-04-02 09:19:55 +0100

    feat: load proxy with kits URL and TLS info

--- src/common/helpers/proxy/setup-proxy.js
+++ src/common/helpers/proxy/setup-proxy.js
@@ -1,6 +1,8 @@
+import tls from 'node:tls'
 import { ProxyAgent, setGlobalDispatcher } from 'undici'
 
 import { config } from '../../../config.js'
+import fetcher from '../fetcher.js'
 import { createLogger } from '../logging/logger.js'
 
 const logger = createLogger()
@@ -9,13 +11,29 @@ const logger = createLogger()
  * If HTTP_PROXY is set setupProxy() will enable it globally
  * for undici http clients.
  */
-export function setupProxy () {
+export function setupProxy() {
   const proxyUrl = config.get('httpProxy')
 
   if (proxyUrl) {
     logger.info('setting up global proxies')
 
+    const requestTls = {
+      host: config.get('kitsConnection.host'),
+      port: config.get('kitsConnection.port'),
+      path: config.get('kitsConnection.path'),
+      // rejectUnauthorized: false,
+      servername: config.get('kitsConnection.host'),
+      secureContext: tls.createSecureContext({
+        key: fetcher.decodeBase64(config.get('kitsConnection.key')),
+        cert: fetcher.decodeBase64(config.get('kitsConnection.cert'))
+      })
+    }
+    logger.info('KITS TLS configuration:', requestTls)
+
+    const proxyAgent = new ProxyAgent({ uri: proxyUrl, requestTls })
     // Undici proxy
-    setGlobalDispatcher(new ProxyAgent(proxyUrl))
+    setGlobalDispatcher(proxyAgent)
+
+    return proxyAgent
   }
 }
