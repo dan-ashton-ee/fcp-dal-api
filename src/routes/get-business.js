9e46a893547889af908f0535c5e473162b1d6327
Robin Knipe <robin.knipe@defra.gov.uk>
2025-04-02 09:20:50 +0100

    fix: refactor to fecth data from KITS

--- src/routes/get-business.js
+++ src/routes/get-business.js
@@ -1,19 +1,34 @@
-import fetcher from '../common/helpers/fetcher.js'
+import { request } from 'undici'
+import { config } from '../config.js'
+// import fetcher from '../common/helpers/fetcher.js'
 import { createLogger } from '../common/helpers/logging/logger.js'
 
 const logger = createLogger()
+const kitsURL = new URL(
+  config.get('kitsConnection.path'),
+  `https://${config.get('kitsConnection.host')}`
+)
+kitsURL.port = config.get('kitsConnection.port')
+logger.info(`Creating new client for ${kitsURL.href}`)
 
 const getBusiness = {
   method: 'GET',
   path: '/get-business/{id}',
-  handler: async (request, h) => {
-    logger.info(`GET /get-business with params: ${JSON.stringify(request.params)}`)
-    const result = await fetcher.fetch(`/organisation/${request.params.id}`)
-    const { name, sbi, orgId } = result.body
+  handler: async (req, h) => {
+    logger.info(`GET /get-business with params: ${JSON.stringify(req.params)}`)
+    const { statusCode, body } = await request(`${kitsURL.href}/organisation/${req.params.id}`,{
+      headers: {
+        'content-type': 'application/json',
+        email: 'Test.User01@defra.gov.uk'
+      }
+    })
+    logger.info(`Response status code: ${statusCode}`)
 
-    logger.info(`business object with keys: ${Object.keys(result.body)}`)
+    const result = await body.json()
+    logger.info(`business object with keys: ${Object.keys(result)}`)
+    const { name, sbi, orgId } = result._data
 
-    h.response({ name, sbi, orgId })
+    return h.response({ name, sbi, orgId })
   }
 }
 
